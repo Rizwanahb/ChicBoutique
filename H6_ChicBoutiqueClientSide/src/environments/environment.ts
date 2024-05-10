0aa76d05b4bd78100f8d6b9e459aa9dbaeaf23dc
Rizwanahb <rizwanahb@yahoo.com>
2024-05-10 19:54:29 +0200

    Add files via upload

---
+++ H6_ChicBoutiqueClientSide/src/environments/environment.ts
@@ -0,0 +1,19 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:7202/api',
+  Client_Id:'AV6EX8kgMXmpB9uNu0fBQR_3FpaYiXI7wnSSUb4RlxiepAvrRdB75JTd6-TGWV-FXw3JKMkfUl7Ie5FQ',
+  Client_Secret:'ECkmg6X0uKtP2-W6adJIvLycsm2Qau71iyZ5qntY7-qTL4VNerTTNEtAZKMSgoNLCkQIgsuDrJfs33tu'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
